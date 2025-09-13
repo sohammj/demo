3429d689cad6b753d6c140cc2e7db551f426bfaf
sohammj <soham0144@gmail.com>
2025-09-14 01:33:00 +0530

    working cms , production ready needs updates to the different pages and the planned animations also on the phone

--- hridmann-web/src/studio/sanity.config.ts
+++ hridmann-web/src/studio/sanity.config.ts
@@ -1,18 +1,14 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemas} from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'Hridmann',
-
   projectId: 'mm6jo5xz',
   dataset: 'production',
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemas,
-  },
+  basePath: '/studio',
+  plugins: [deskTool(), visionTool()],
+  schema: { types: schemas },
 })
