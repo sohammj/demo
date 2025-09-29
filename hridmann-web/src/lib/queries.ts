bf01e0c3b01ccb729ad756b68c85a6db00c6815c
Sohammj <soham0144@gmail.com>
2025-09-29 13:05:38 +0530

    final

--- hridmann-web/src/lib/queries.ts
+++ hridmann-web/src/lib/queries.ts
@@ -1,22 +1,22 @@
 // import { groq } from 'next-sanity'
 
-// export const HOME_QUERY = groq`*[_type == "home"][0]{
-//   heroTitle,
-//   heroSubtitle,
-//   heroBadge,
-//   aboutTitle,
-//   aboutBody,
-//   portrait,
-//   aboutPortrait,
-//   focusAreas,
-//   certifications,
-//   servicesTitle,
-//   servicesSubtitle,
-//   testimonialsTitle,
-//   testimonialsSubtitle,
-//   contactTitle,
-//   contactHelpText,
-// }`
+export const HOME_QUERY = groq`*[_type == "home"][0]{
+  heroTitle,
+  heroSubtitle,
+  heroBadge,
+  aboutTitle,
+  aboutBody,
+  portrait,
+  aboutPortrait,
+  focusAreas,
+  certifications,
+  servicesTitle,
+  servicesSubtitle,
+  testimonialsTitle,
+  testimonialsSubtitle,
+  contactTitle,
+  contactHelpText,
+}`
 
 // export const SERVICES_QUERY = groq`*[_type == "service"] | order(title asc){
 //   _id,
@@ -26,12 +26,12 @@
 //   slug,
 // }`
 
-// export const TESTIMONIALS_QUERY = groq`*[_type == "testimonial"] | order(_createdAt desc){
-//   name,
-//   role,
-//   message,
-//   photo
-// }`
+export const TESTIMONIALS_QUERY = groq`*[_type == "testimonial"] | order(_createdAt desc){
+  name,
+  role,
+  message,
+  photo
+}`
 
 // export const SETTINGS_QUERY = groq`*[_type == "settings"][0]{
 //   siteName,
